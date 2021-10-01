b9eac1a9d0db2c9c37c2e33bc8aa9705e16570ba
NoNameBlank <86839934+NoNameBlank@users.noreply.github.com>
2021-10-01 13:27:20 +0200

    Projektdateien hinzufügen.

---
+++ KryptoWarZV0.5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KryptoWarZV0._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
